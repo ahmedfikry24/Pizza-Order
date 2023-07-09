d8d373a16f7b9876c81a23a79fa0567b29238785
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-09 23:51:35 +0300

    add appbar

---
+++ app/src/main/java/com/example/pizzaorder/composables/AppBar.kt
@@ -0,0 +1,41 @@
+package com.example.pizzaorder.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import com.example.pizzaorder.R
+import com.example.pizzaorder.ui.theme.space16
+import com.example.pizzaorder.ui.theme.text18
+
+@Composable
+fun AppBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(space16),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_back),
+            contentDescription = stringResource(
+                R.string.back_icon
+            )
+        )
+        Text(text = stringResource(R.string.pizza), fontSize = text18, color = Color.Black)
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_favorite),
+            contentDescription = stringResource(
+                R.string.favorites
+            )
+        )
+    }
+}
