9cc7a65885d34385428afbdb7824b4a9ff3945c4
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-09 23:29:32 +0300

    add dimens

---
+++ app/src/main/java/com/example/pizzaorder/ui/theme/Dimen.kt
@@ -0,0 +1,13 @@
+package com.example.pizzaorder.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val space4 = 4.dp
+val space8 = 8.dp
+val space12 = 12.dp
+val space16 = 16.dp
+val space18 = 18.dp
+val space20 = 20.dp
+
+val text18 = 18.sp
