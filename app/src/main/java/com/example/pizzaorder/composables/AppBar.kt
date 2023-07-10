a632da43e3647e0929c96be1311659ce293ef053
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-10 20:41:55 +0300

    update AppBar

--- app/src/main/java/com/example/pizzaorder/composables/AppBar.kt
+++ app/src/main/java/com/example/pizzaorder/composables/AppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import com.example.pizzaorder.R
 import com.example.pizzaorder.ui.theme.space16
 import com.example.pizzaorder.ui.theme.text18
@@ -30,7 +31,7 @@ fun AppBar() {
                 R.string.back_icon
             )
         )
-        Text(text = stringResource(R.string.pizza), fontSize = text18, color = Color.Black)
+        Text(text = stringResource(R.string.pizza), fontSize = text18, color = Color.Black,fontWeight= FontWeight.Black)
         Image(
             imageVector = ImageVector.vectorResource(R.drawable.ic_favorite),
             contentDescription = stringResource(
