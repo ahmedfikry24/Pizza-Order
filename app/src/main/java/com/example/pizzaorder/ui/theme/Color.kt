0fa630dbb2bc4174c4836dce8f795c60535c4695
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-10 20:41:44 +0300

    update colors & dimes

--- app/src/main/java/com/example/pizzaorder/ui/theme/Color.kt
+++ app/src/main/java/com/example/pizzaorder/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val white = Color(0xFFFFFFFF)
+val white87 = Color(0xDEFFFFFF)
+
+val lightGreen = Color(0xFF86FF8B)
+
+val brown = Color(0xFF352826)
--- app/src/main/java/com/example/pizzaorder/ui/theme/Dimen.kt
+++ app/src/main/java/com/example/pizzaorder/ui/theme/Dimen.kt
@@ -9,5 +9,18 @@ val space12 = 12.dp
 val space16 = 16.dp
 val space18 = 18.dp
 val space20 = 20.dp
+val space40 = 40.dp
+val space60 = 20.dp
 
+val text24 = 24.sp
 val text18 = 18.sp
+val text16 = 16.sp
+val text14 = 14.sp
+
+val size40 = 40.dp
+val size50 = 50.dp
+val size56 = 56.dp
+val size70 = 70.dp
+val size168 = 168.dp
+val size250 = 250.dp
+val size200 = 200.dp
