9e8608703e0d964bcbbe44cca59362bcacec4491
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-19 03:58:39 +0300

    update dimens

--- app/src/main/java/com/example/pizzaorder/ui/theme/Dimen.kt
+++ app/src/main/java/com/example/pizzaorder/ui/theme/Dimen.kt
@@ -5,23 +5,19 @@ import androidx.compose.ui.unit.sp
 
 val space4 = 4.dp
 val space8 = 8.dp
-val space12 = 12.dp
 val space16 = 16.dp
-val space18 = 18.dp
 val space20 = 20.dp
 val space40 = 40.dp
-val space60 = 20.dp
 
 val text24 = 24.sp
 val text18 = 18.sp
 val text16 = 16.sp
 val text14 = 14.sp
 
-val size40 = 40.dp
-val size20 = 20.dp
 val size50 = 50.dp
 val size56 = 56.dp
 val size70 = 70.dp
 val size168 = 168.dp
 val size250 = 250.dp
-val size200 = 200.dp
+val size230 = 230.dp
+val size180 = 180.dp
